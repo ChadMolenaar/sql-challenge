922ae39a846ef44666f8439b19abc81f62a886f4
ChadMolenaar <74281700+ChadMolenaar@users.noreply.github.com>
2021-03-09 12:27:25 +1100

    Delete 09-SQL/Schema directory

--- 09-SQL/Schema/part_1.sql
+++
@@ -1,5 +0,0 @@
--- List the following details of each employee, 
--- employee number, first name, last name, sex, salary 
-SELECT employees.emp_no, first_name, last_name, sex, salary
-from employees
-left join salaries on employees.emp_no = salaries.emp_no;
